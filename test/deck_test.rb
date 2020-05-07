07fa9f4b5cc8854bb86d6b690d04a71fc338eae8
Nick Edwin <nickedwin85@gmail.com>
2020-05-07 12:49:02 -0600

    Finalized deck tests

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -7,34 +7,45 @@ require 'pry'
 class DeckTest < Minitest::Test
 
   def setup
-    card1 = Card.new(:diamond, 'Queen', 12)
-    card2 = Card.new(:spade, '3', 3)
-    card3 = Card.new(:heart, 'Ace', 14)
-    card4 = Card.new(:club, '5', 5)
-    @cards = [@card1, @card2, @card3]
-    @deck = Deck.new(@cards)
+    @card1 = Card.new(:diamond, 'Queen', 12)
+    @card2 = Card.new(:spade, '3', 3)
+    @card3 = Card.new(:heart, 'Ace', 14)
+    @card4 = Card.new(:club, '5', 5)
+    cards = [@card1, @card2, @card3]
+    @deck = Deck.new(cards)
   end
 
 
   def test_it_exists
     deck = @deck
- 
+
     assert_instance_of Deck, @deck
   end
 
   def test_it_has_a_rank
-    deck = @deck
+
+
     assert_equal 12, @deck.rank_of_card_at(0)
     assert_equal 14, @deck.rank_of_card_at(2)
   end
 
   def test_its_high_ranking
-skip
-    assert_equal 2, high_rank.count == 2
+
+    assert_equal [@card1, @card3], @deck.high_ranking_cards
   end
 
   def test_percent_high_ranking
-    skip
+
     assert_equal 66.67, @deck.percent_high_ranking
+  end
+
+  def test_it_can_remove_cards
+    @deck.remove_card
+    assert_equal [@card2, @card3], @deck.cards
+  end
+
+  def test_it_can_add_cards
+    @deck.add_card(@card4)
+    assert_equal [@card1, @card2, @card3, @card4], @deck.cards
   end
 end
