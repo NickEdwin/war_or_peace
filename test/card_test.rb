f19079cbe1a9bdf8ef87f1d79f6e4b18af1f50be
Nick Edwin <nickedwin85@gmail.com>
2020-05-06 22:12:05 -0600

    Fixed errors in test

--- test/card_test.rb
+++ test/card_test.rb
@@ -1,6 +1,7 @@
 require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/card'
+require 'pry'
 
 class CardTest < Minitest::Test
   def setup
@@ -15,7 +16,7 @@ class CardTest < Minitest::Test
 
   def test_it_has_readable_attributes
     card = Card.new(:diamond, 'Queen', 12)
-    
+
     assert_equal :diamond, card.suit
     assert_equal 'Queen', card.value
     assert_equal 12, card.rank
