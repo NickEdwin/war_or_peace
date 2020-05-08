1776a3a981790059a4cf231641cb7f128da42476
Nick Edwin <nickedwin85@gmail.com>
2020-05-07 23:23:21 -0600

    Updated until 'award spoils test'

---
+++ test/turn_test.rb
@@ -0,0 +1,55 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/card'
+require './lib/deck'
+require './lib/player'
+require './lib/turn'
+
+class Turntest < Minitest::Test
+  def setup
+    @card1 = Card.new(:heart, 'Jack', 11)
+    @card2 = Card.new(:heart, '10', 10)
+    @card3 = Card.new(:heart, '9', 9)
+    @card4 = Card.new(:diamond, 'Jack', 11)
+    @card5 = Card.new(:heart, '8', 8)
+    @card6 = Card.new(:diamond, 'Queen', 12)
+    @card7 = Card.new(:heart, '3', 3)
+    @card8 = Card.new(:diamond, '2', 2)
+
+    @deck1 = Deck.new([@card1, @card2, @card5, @card8])
+    @deck2 = Deck.new([@card3, @card4, @card6, @card7])
+
+    @player1 = Player.new("Megan", @deck1)
+    @player2 = Player.new("Aurora", @deck2)
+
+    @turn = Turn.new(@player1, @player2)
+  end
+
+  def test_it_has_player1
+
+    assert_equal @player1, @turn.player1
+  end
+
+
+  def test_it_has_correct_turn_type
+
+    assert_equal :basic, @turn.type
+  end
+
+  def test_it_has_a_winner
+
+    assert_equal @player1 , @turn.winner
+  end
+
+  def test_it_has_spoils_of_war
+
+    assert_equal @spoils_of_war = [@card1, @card3], @turn.pile_cards
+    # test below for :war pile_cards scenario
+    # assert_equal @spoils_of_war = [@card1, @card2, @card5, @card3, @card4, @card6], @turn.pile_cards
+  end
+
+  def test_award_spoils
+    winner = @turn.winner
+    assert_equal 5, winner.deck.cards.count
+  end
+end
