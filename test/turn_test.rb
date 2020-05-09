d2b5eb993769ea71d7efe1f3d2bacc028e72aeed
Nick Edwin <nickedwin85@gmail.com>
2020-05-09 13:42:05 -0600

    Finished iteration 2 turn tests

--- test/turn_test.rb
+++ test/turn_test.rb
@@ -4,6 +4,7 @@ require './lib/card'
 require './lib/deck'
 require './lib/player'
 require './lib/turn'
+require 'pry'
 
 class Turntest < Minitest::Test
   def setup
@@ -25,15 +26,33 @@ class Turntest < Minitest::Test
     @turn = Turn.new(@player1, @player2)
   end
 
-  def test_it_has_player1
+  def test_it_has_player
+    @card1 = Card.new(:heart, 'Jack', 11)
+    @card2 = Card.new(:heart, '10', 10)
+    @card3 = Card.new(:heart, '9', 9)
+    @card4 = Card.new(:diamond, 'Jack', 11)
+    @card5 = Card.new(:heart, '8', 8)
+    @card6 = Card.new(:diamond, 'Queen', 12)
+    @card7 = Card.new(:heart, '3', 3)
+    @card8 = Card.new(:diamond, '2', 2)
+
+    @deck1 = Deck.new([@card1, @card2, @card5, @card8])
+    @deck2 = Deck.new([@card3, @card4, @card6, @card7])
+
+    @player1 = Player.new("Megan", @deck1)
+    @player2 = Player.new("Aurora", @deck2)
+
+    @turn = Turn.new(@player1, @player2)
 
     assert_equal @player1, @turn.player1
+    assert_equal @player2, @turn.player2
+
   end
 
 
   def test_it_has_correct_turn_type
 
-    assert_equal :basic, @turn.type
+    assert_equal :basic, @turn.turn_type
   end
 
   def test_it_has_a_winner
@@ -49,7 +68,23 @@ class Turntest < Minitest::Test
   end
 
   def test_award_spoils
-    winner = @turn.winner
-    assert_equal 5, winner.deck.cards.count
+    @card1 = Card.new(:heart, 'Jack', 11)
+    @card2 = Card.new(:heart, '10', 10)
+    @card3 = Card.new(:heart, '9', 9)
+    @card4 = Card.new(:diamond, 'Jack', 11)
+    @card5 = Card.new(:heart, '8', 8)
+    @card6 = Card.new(:diamond, 'Queen', 12)
+    @card7 = Card.new(:heart, '3', 3)
+    @card8 = Card.new(:diamond, '2', 2)
+
+    @deck1 = Deck.new([@card1, @card2, @card5, @card8])
+    @deck2 = Deck.new([@card3, @card4, @card6, @card7])
+
+    @player1 = Player.new("Megan", @deck1)
+    @player2 = Player.new("Aurora", @deck2)
+
+    @turn = Turn.new(@player1, @player2)
+
+    assert_equal @player1, @turn.winner
   end
 end
